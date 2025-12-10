a83d91c49d0ff59c3ac3decd58f26548dd318d1f
Chandana <rebbachandana54@gmail.com>
2025-12-10 22:29:59 +0000

    s

---
+++ 16_logs.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+USER_ID=$(id -u)
+R="\e[31m"
+G="\e[32m"
+Y="\e[33m"
+N="\e[0m"
+FOLDER_PATH="/var/log/Scripting"
+script_name=$(echo $0 | cut -d "." -f1)
+file_name ="$FOLDER_PATH/$script_name.log"
+if [ $USER_ID -ne 0 ] ; then
+    echo -e "$R ERROR : Please run this script with root access $N"
+    exit 1
+fi
+
+Validate(){
+    if [ $1 -ne 0 ]; then 
+    echo -e " ERROR : Installing $2 is $R failure $N"
+    exit 1
+else
+    echo -e "Installing $2 is $G SUCCESS $N"
+fi
+}
+dnf list installed mysql
+if [ $? -ne 0 ]; then
+    dnf install mysql-server -y
+    Validate $? MYSQL
+else 
+    echo -e " MYSQL Already exists....$Y Skipping $N...."
+fi
+
+dnf list installed nginx
+if [ $? -ne 0 ]; then
+    dnf install nginx -y
+    Validate $? nginx
+else 
+    echo -e " nginx Already exists....$Y Skipping $N...."
+fi
