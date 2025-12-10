bb234f9d1ee0507d6fa552fa78c81cb83e95b304
Chandana <rebbachandana54@gmail.com>
2025-12-10 21:36:09 +0000

    s

--- condition.sh
+++ condition.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 n =$1
-if [ n -lt 10 ] then
+if [ n -lt 10 ] ; then
   echo "Given Number $n is less than 10"
-elif [ n -gt 10 ] then
+elif [ n -gt 10 ] ; then
   echo "Given Number $n is greater than 10"
 else
   echo "number $n is equal to 10"
