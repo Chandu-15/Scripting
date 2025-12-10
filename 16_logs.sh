77bcd1ff4101d1d4202220105bfe023cc780b28e
Chandana <rebbachandana54@gmail.com>
2025-12-10 22:44:03 +0000

    s

--- 16_logs.sh
+++ 16_logs.sh
@@ -6,32 +6,34 @@ Y="\e[33m"
 N="\e[0m"
 FOLDER_PATH="/var/log/Scripting"
 script_name=$(echo $0 | cut -d "." -f1)
-file_name ="$FOLDER_PATH/$script_name.log"
+log_file="$FOLDER_PATH/$script_name.log"
+mkdir -p $FOLDER_PATH
+echo "script execution started at $(date)" | tee -a $log_file
 if [ $USER_ID -ne 0 ] ; then
-    echo -e "$R ERROR : Please run this script with root access $N"
+    echo -e "$R ERROR : Please run this script with root access $N" | tee -a $log_file
     exit 1
 fi
 
 Validate(){
     if [ $1 -ne 0 ]; then 
-    echo -e " ERROR : Installing $2 is $R failure $N"
+    echo -e " ERROR : Installing $2 is $R failure $N" | tee -a $log_file
     exit 1
 else
-    echo -e "Installing $2 is $G SUCCESS $N"
+    echo -e "Installing $2 is $G SUCCESS $N" | tee -a $log_file
 fi
 }
-dnf list installed mysql
+dnf list installed mysql &>>$log_file
 if [ $? -ne 0 ]; then
-    dnf install mysql-server -y
+    dnf install mysql-server -y &>> log_file
     Validate $? MYSQL
 else 
-    echo -e " MYSQL Already exists....$Y Skipping $N...."
+    echo -e " MYSQL Already exists....$Y Skipping $N...." | tee -a $log_file
 fi
 
-dnf list installed nginx
+dnf list installed nginx &>>$log_file
 if [ $? -ne 0 ]; then
-    dnf install nginx -y
+    dnf install nginx -y &>>$log_file
     Validate $? nginx
 else 
-    echo -e " nginx Already exists....$Y Skipping $N...."
+    echo -e " nginx Already exists....$Y Skipping $N...." | tee -a $log_file
 fi
