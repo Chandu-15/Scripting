39b616996a2876c4ef560cce1fa94a1a7c4a8025
Chandana <rebbachandana54@gmail.com>
2025-12-10 19:38:49 +0000

    s

---
+++ var.sh
@@ -0,0 +1,8 @@
+#!/bin/bash
+n1=chandu
+n2=ram
+
+echo "$n1 : Hi , How are you n2?"
+echo "$n2 : I am fine...how is your healing going on $n1?"
+echo "$n1 : it is going fine...i am finding peace now "
+echo "$n2 : it is so good to hear from you "
