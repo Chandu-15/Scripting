b5896af08f6e02fe9fe135c174c23c21c5cacc38
Chandana <rebbachandana54@gmail.com>
2025-12-10 21:54:50 +0000

    s

---
+++ installation.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+USER_ID =$(id -u)
+
+if [ $USER_ID -ne 0 ] ; then
+    echo "ERROR : Please run this script with root access"
+fi
+
+dnf install mysql-server -y
+if [ $? -ne 0 ]; then 
+    echo "ERROR : Installing mysql is failure"
+else
+    echo "Installing mysql is SUCCESS"
+fi
+dnf install python3 -y
+if [ $? -ne 0 ]; then 
+    echo "ERROR : Installing python is failure"
+else
+    echo "Installing python is SUCCESS"
+fi
